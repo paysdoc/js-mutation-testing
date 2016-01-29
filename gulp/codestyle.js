26f3e8781dd4e910106597dc7703e1f8bd7423cb
martin <paysdoc@gmail.com>
2016-01-29 23:23:04 +0100

    replaced commit hook with push hook (where it matters)

--- gulp/codestyle.js
+++ gulp/codestyle.js
@@ -9,7 +9,7 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 gulp.task('lint', function() {
 
-    gulp.src('src/**/*.js')
+    gulp.src(['src/**/*.js', 'test/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
